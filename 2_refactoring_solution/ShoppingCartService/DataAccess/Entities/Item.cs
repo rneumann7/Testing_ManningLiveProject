1f1362c2dc175a6ad55631c1e1a9cfc025ca17ee
Raoul Neumann <raoul.neumann@gmail.com>
2024-03-28 16:20:16 +0100

    Solution for ex 2

---
+++ 2_refactoring_solution/ShoppingCartService/DataAccess/Entities/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoppingCartService.DataAccess.Entities
+{
+    public class Item
+    {
+        public string ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public double Price { get; set; }
+
+        public uint Quantity { get; set; }
+    }
+}
---
+++ 2_refactoring_solution/ShoppingCartServiceTests/Builders/ItemBuilder.cs
@@ -0,0 +1,28 @@
+﻿using ShoppingCartService.DataAccess.Entities;
+
+namespace ShoppingCartServiceTests.Builders
+{
+    /// <summary>
+    /// This is an example for using the Object Mother pattern
+    /// You can use 'using static' to make your code more readable.
+    /// </summary>
+
+    public class ItemBuilder
+    {
+        public static Item CreateItem(
+            string productId = "prod-1",
+            string productName = "product 1",
+            double price = 10.0,
+            uint quantity = 1)
+        {
+
+            return new()
+            {
+                ProductId = productId,
+                ProductName = productName,
+                Price = price,
+                Quantity = quantity
+            };
+        }
+    }
+}
